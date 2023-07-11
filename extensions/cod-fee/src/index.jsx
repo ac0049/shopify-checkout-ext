8c675dd5faec2ab2dcb5e120424127f4079d327b
Roman <roman.gavrilov.0309@gmail.com>
2023-07-11 16:17:07 -0400

    COD Fee

--- extensions/cod-fee/src/index.jsx
+++ extensions/cod-fee/src/index.jsx
@@ -4,43 +4,52 @@ import {
   Banner,
   useApplyCartLinesChange,
   useSelectedPaymentOptions,
-  useCartLines
+  useCartLines,
+  useLocalizationCountry
 } from '@shopify/checkout-ui-extensions-react';
 
 render('Checkout::Dynamic::Render', () => <App />);
 
 function App() {
-  const cartLines = useCartLines();
-  const addProductLine = useApplyCartLinesChange();
+  const cartLines = useCartLines(),
+        addProductLine = useApplyCartLinesChange(),
+        country = useLocalizationCountry();
   const paymentOptions = useSelectedPaymentOptions();
-  const feeId = "gid://shopify/ProductVariant/45529564479783";
+  let feeId = "gid://shopify/ProductVariant/";
+  if(country.isoCode == "SL")
+    feeId += "45546242736423"
+  else if(country.isoCode == "IT")
+    feeId += "45546242769191"
+  else {
+    feeId += "45546242801959"
+  }
 
   useEffect(async () => {
-    // if(paymentOptions[0].type == "paymentOnDelivery") {
-    //   if(cartLines.filter((line) => line.merchandise.id == feeId).length == 0) {
-    //     await addProductLine({
-    //       type: 'addCartLine',
-    //       merchandiseId: feeId,
-    //       quantity: 1
-    //     });
-    //   } else {
-    //     const feedLineItem = cartLines.filter((line) => line.merchandise.id == feeId);
-    //     await addProductLine({
-    //       type: 'removeCartLine',
-    //       id: feedLineItem.id,
-    //       quantity: 1
-    //     });
-    //   }
-    // } else {
-    //   if(cartLines.filter((line) => line.merchandise.id == feeId).length != 0) {
-    //     const feedLineItem = cartLines.filter((line) => line.merchandise.id == feeId)[0];
-    //     await addProductLine({
-    //       type: 'removeCartLine',
-    //       id: feedLineItem.id,
-    //       quantity: 1
-    //     });
-    //   }
-    // }
+    if(paymentOptions[0].type == "paymentOnDelivery") {
+      if(cartLines.filter((line) => line.merchandise.id == feeId).length == 0) {
+        await addProductLine({
+          type: 'addCartLine',
+          merchandiseId: feeId,
+          quantity: 1
+        });
+      } else {
+        const feedLineItem = cartLines.filter((line) => line.merchandise.id == feeId);
+        await addProductLine({
+          type: 'removeCartLine',
+          id: feedLineItem.id,
+          quantity: 1
+        });
+      }
+    } else {
+      if(cartLines.filter((line) => line.merchandise.id == feeId).length != 0) {
+        const feedLineItem = cartLines.filter((line) => line.merchandise.id == feeId)[0];
+        await addProductLine({
+          type: 'removeCartLine',
+          id: feedLineItem.id,
+          quantity: 1
+        });
+      }
+    }
   });
 
   return (
--- extensions/show-discounted-price/src/index.jsx
+++ extensions/show-discounted-price/src/index.jsx
@@ -1,52 +1,13 @@
 import React, {useEffect, useState} from 'react';
 import {
-  useExtensionApi,
-  render,
-  Banner,
-  useCartLines
+  render
 } from '@shopify/checkout-ui-extensions-react';
 
 render('Checkout::Dynamic::Render', () => <App />);
 
 function App() {
-  const cartLines = useCartLines();
-  console.log(cartLines[0].merchandise.product.id);
-  const {query} = useExtensionApi();
-  const [data, setData] = useState();
-
-  useEffect(() => {
-    query(`
-      query getProductById($id: ID!) {
-        product(id: $id) {
-          title,
-           variants(first:10){
-            nodes{
-              id,
-              compareAtPrice {
-                amount
-              },
-              price {
-                amount
-              }
-            }
-          }
-        }
-      }`,
-      {
-       variables: {id: cartLines[0].merchandise.product.id} 
-      }
-    )
-      .then(({data, errors}) => {
-        console.log(errors);
-        setData(data);
-      })
-      .catch(console.error);
-  }, [query]);
-
-  console.log(data);
-  
   return (
-    <Banner title="show-discounted-price">
-    </Banner>
+    <>
+    </>
   );
 }
